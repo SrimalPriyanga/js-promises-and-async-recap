03203fc0068d39d28f390405f5ed9ae94fc5f6f8
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-17 13:22:42 +0530

    custom promise creation

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,99 @@
 import setText, { appendText } from "./results.mjs";
 
-export function timeout(){
+export function timeout() {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Timeout!");
+    }, 1500);
+  });
+
+  wait.then((text) => setText(text));
 }
 
-export function interval(){
+export function interval() {
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait
+    .then((text) => setText(text))
+    .finally(() => appendText(` -- Done ${counter}`)); // promise still running
 }
 
-export function clearIntervalChain(){
+export function clearIntervalChain() {
+  let counter = 0;
+  let interval;
+  const wait = new Promise((resolve) => {
+    interval = setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait.then((text) => setText(text)).finally(() => clearInterval(interval)); // clear promise
 }
 
-export function xhr(){
+export function xhr() {
+  let request = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:3000/users/7");
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => reject("Request Failed");
+    xhr.send();
+  });
+
+  request.then((result) => setText(result)).catch((reason) => setText(reason));
+}
+export function allPromises() {
+  let categories = axios.get("http://localhost:3000/itemCategories");
+  let statuses = axios.get("http://localhost:3000/orderStatuses");
+  let userTypes = axios.get("http://localhost:3000/userTypes");
+  let addressTypes = axios.get("http://localhost:3000/addressTypes"); // error accouring call
+
+  Promise.all([categories, statuses, userTypes, addressTypes])
+    .then(([cat, sta, use, add]) => {
+      setText("");
+      appendText(JSON.stringify(cat.data));
+      appendText(JSON.stringify(sta.data));
+      appendText(JSON.stringify(use.data));
+      appendText(JSON.stringify(add.data));
+    })
+    .catch((err) => setText(err));
 }
 
-export function allPromises(){
+export function allSettled() {
+  let categories = axios.get("http://localhost:3000/itemCategories");
+  let statuses = axios.get("http://localhost:3000/orderStatuses");
+  let userTypes = axios.get("http://localhost:3000/userTypes");
+  let addressTypes = axios.get("http://localhost:3000/addressTypes"); // error accouring call
+
+  Promise.allSettled([categories, statuses, userTypes, addressTypes])
+    .then((values) => {
+      console.log(values);
+      let results = values.map((v) => {
+        if (v.status == "fulfilled")
+          return `FULFILLED: ${JSON.stringify(v.value.data[0])} `;
+        else return `REJECTED: ${v.reason.message} `;
+      });
+      setText(results);
+    })
+    .catch((err) => setText(err));
 }
 
-export function allSettled(){
-}
-
-export function race(){
+export function race() {
+  let users = axios.get("http://localhost:3000/users");
+  let backup = axios.get("http://localhost:3001/users");
+  Promise.race([users, backup])
+    .then((users) => setText(JSON.stringify(users.data)))
+    .catch((reason) => setText(reason));
 }
